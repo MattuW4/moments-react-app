c775cfca611ccd3088ba48397bc74eedcf99e0b7
Matt Waters <enjoi_waters@hotmail.com>
2024-02-13 16:52:07 +0000

    Create 404 page and connect up

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,7 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
-import NavBar from "../NavBar";
+import { CurrentUserProvider } from "../contexts/CurrentUserContext";
+import NavBar from "./NavBar";
 
 test("renders NavBar", () => {
     render(
